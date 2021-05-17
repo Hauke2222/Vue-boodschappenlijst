b3e8ab30711de26ebcd580aefedb2b7acb6eb67e
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2021-05-17 15:59:26 +0200

    added table + price methods

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <table>
-      <!-- <TableRow :grocery="GroceryList[index]" msg="Vue boodschappenlijst" v-for="(n, index) in GroceryList" :key="index"/> -->
-      </table>
-    <div v-for="(g) in GroceryList" v-bind:key="g">
-      {{ 'Naam: ' + g.name + ', Prijs: ' + g.price + ', Aantal: ' + g.quantity}}
-    </div>
+    <table style="width:50%">
+      <thead>
+        <tr>
+          <td>Naam</td>
+          <td>Prijs</td>
+          <td>Aantal</td>
+          <td>Totaal</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(g) in GroceryList" v-bind:key="g">
+            <td>{{ g.name }}</td>
+            <td>{{ g.price }}</td>
+            <td><input type="number" pattern=" 0+\.[0-9]*[1-9][0-9]*$" min="0" v-bind:value="g.quantity"></td>
+            <td>{{ g.subTotal }}</td>
+          </tr>
+          <tr>
+            <td><b>Totaal:</b></td>
+            <td>x</td>
+            <td></td>
+            <td id="totalCost"><b>{{ Totalprice }}</b></td>
+          </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -21,34 +39,33 @@ export default {
   },
 
   methods: {
-    // FillGroceryObjectList() {
-    //   for (let i = 0; i < this.GroceryList.length; i++) {
-    //     this.GroceryObjectList.push(
-    //       new Grocery(
-    //         this.GroceryNamesAndPrices[i].name, 
-    //         this.GroceryList[i].price, 
-    //         this.GroceryList[i].quantity
-    //       )
-    //     );
-    //   } 
-    // }
-    CalculateSubTotal() {
-      //this.GroceryList[i].quantity * this.GroceryList[i].price
+
+    IncreaseQuantity() {
 
     },
-    CalculateTotal() {
 
+    CalculateSubTotal() {
+      for (let i = 0; i < this.GroceryList.length; i++) {
+        this.GroceryList[i].subTotal = this.GroceryList[i].quantity * this.GroceryList[i].price
+      }
+    },
+    CalculateTotal() {
+      this.Totalprice = 0;
+      for (let i = 0; i < this.GroceryList.length; i++) {
+        this.Totalprice += this.GroceryList[i].quantity * this.GroceryList[i].price
+      }
     }
   },
 
   data() {
     return {
       //GroceryObjectList: [],
+      Totalprice: 999,
       GroceryList: [
-        {name: 'Brood', price: '1,00', quantity: 1},
-        {name: 'Broccoli', price: '0,99', quantity: 1},
-        {name: 'Krentenbollen', price: '1,20', quantity: 1},
-        {name: 'Noten', price: '2,99', quantity: 1},
+        {name: 'Brood', price: '1,00', quantity: 1, subTotal: '1,00'},
+        {name: 'Broccoli', price: '0,99', quantity: 1, subTotal: '0,99'},
+        {name: 'Krentenbollen', price: '1,20', quantity: 1, subTotal: '1,20'},
+        {name: 'Noten', price: '2,99', quantity: 1, subTotal: '2,99'},
       ],
     }
   },
@@ -63,5 +80,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
 }
+table {
+    border: 1px solid black; 
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  th, td {
+    border: 1px solid lightgray;
+    text-align: center;
+  }
+  #totalCost{
+    background-color: lightgray;
+  }
 </style>

49dbfe573393a3e77a3d7c7f4f9209bf7119bb68
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2021-05-19 15:51:31 +0200

    added getter to Grocery class

--- src/grocery.js
+++ src/grocery.js
@@ -1,7 +1,10 @@
-class Grocery {
+export default class Grocery {
     constructor(name, price, quantity) {
         this.name = name;
         this.price = price; 
         this.quantity = quantity;
+    }
+    get subTotal() {
+        return parseFloat(this.price.replace(',', '.').slice(1)) * parseInt(this.quantity)
     }
 }

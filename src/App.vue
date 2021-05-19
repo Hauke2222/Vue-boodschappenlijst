2716a927ebc8bc4b86d1bca7b215ed9585f4faae
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2021-05-19 15:54:24 +0200

    using Grocery class to create objects

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <table style="width:50%">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <table style="width: 50%">
       <thead>
         <tr>
           <td>Naam</td>
@@ -11,18 +11,28 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(g) in GroceryList" v-bind:key="g">
-            <td>{{ g.name }}</td>
-            <td>{{ g.price }}</td>
-            <td><input type="number" pattern=" 0+\.[0-9]*[1-9][0-9]*$" min="0" v-bind:value="g.quantity"></td>
-            <td>{{ g.subTotal }}</td>
-          </tr>
-          <tr>
-            <td><b>Totaal:</b></td>
-            <td>x</td>
-            <td></td>
-            <td id="totalCost"><b>{{ Totalprice }}</b></td>
-          </tr>
+        <tr v-for="g in GroceryList" v-bind:key="g">
+          <td>{{ g.name }}</td>
+          <td>{{ g.price }}</td>
+          <td>
+            <input
+              type="number"
+              pattern=" 0+\.[0-9]*[1-9][0-9]*$"
+              min="0"
+              v-bind:value="g.quantity"
+              @change="IncreaseQuantity()"
+            />
+          </td>
+          <td>{{ g.subTotal }}</td>
+        </tr>
+        <tr>
+          <td><b>Totaal:</b></td>
+          <td>x</td>
+          <td></td>
+          <td id="totalCost">
+            <b>{{ Totalprice }}</b>
+          </td>
+        </tr>
       </tbody>
     </table>
   </div>
@@ -30,46 +40,40 @@
 
 <script>
 //import TableRow from './components/TableRow.vue'
-//import Grocery from './grocery.js'
+import Grocery from "./grocery.js";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     //TableRow
   },
 
   methods: {
-
-    IncreaseQuantity() {
-
-    },
-
-    CalculateSubTotal() {
-      for (let i = 0; i < this.GroceryList.length; i++) {
-        this.GroceryList[i].subTotal = this.GroceryList[i].quantity * this.GroceryList[i].price
-      }
+    IncreaseQuantity() { 
     },
     CalculateTotal() {
       this.Totalprice = 0;
       for (let i = 0; i < this.GroceryList.length; i++) {
-        this.Totalprice += this.GroceryList[i].quantity * this.GroceryList[i].price
+        this.Totalprice += this.GroceryList[i].price;
       }
-    }
+    },
+  },
+  created() {
+    console.log(this.GroceryList);
   },
 
   data() {
     return {
-      //GroceryObjectList: [],
       Totalprice: 999,
       GroceryList: [
-        {name: 'Brood', price: '1,00', quantity: 1, subTotal: '1,00'},
-        {name: 'Broccoli', price: '0,99', quantity: 1, subTotal: '0,99'},
-        {name: 'Krentenbollen', price: '1,20', quantity: 1, subTotal: '1,20'},
-        {name: 'Noten', price: '2,99', quantity: 1, subTotal: '2,99'},
+        new Grocery("Brood", "1,00", 1),
+        new Grocery("Broccoli", "0,99", 1),
+        new Grocery("Krentenbollen", "1,20", 1),
+        new Grocery("Noten", "2,99", 1),
       ],
-    }
+    };
   },
-}
+};
 </script>
 
 <style>
@@ -80,20 +84,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  
 }
 table {
-    border: 1px solid black; 
-    text-align: center;
-    margin-left: auto;
-    margin-right: auto;
-  }
+  border: 1px solid black;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+}
 
-  th, td {
-    border: 1px solid lightgray;
-    text-align: center;
-  }
-  #totalCost{
-    background-color: lightgray;
-  }
+th,
+td {
+  border: 1px solid lightgray;
+  text-align: center;
+}
+#totalCost {
+  background-color: lightgray;
+}
 </style>
